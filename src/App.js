226074a2a0ec00720d29925e77b91eb850e88abb
LENA EIVY <applena@gmail.com>
2019-11-05 16:45:34 -0800

    fixed spelling mistake

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Dollar Cost Avergering Investing in Bitcoin</h1>
+       <h1>Dollar Cost Averaging Investing in Bitcoin</h1>
       </header>
       <TimeForm></TimeForm>
       <Chart></Chart>
