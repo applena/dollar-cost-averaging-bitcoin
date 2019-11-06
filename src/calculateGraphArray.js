209a52804dadef3afad7eec3cbf668c3e93ed95d
LENA EIVY <applena@gmail.com>
2019-11-05 17:53:59 -0800

    added principal line

--- src/calculateGraphArray.js
+++ src/calculateGraphArray.js
@@ -1,14 +1,17 @@
 function calculateGraphArray(arr, invest){
-  let data = {date:[], price:[], shares:[], invenstment:[]};
+  let data = {date:[], price:[], shares:[], invenstment:[], principal:[]};
 
   // populate the date and price from the API
+  let temp=0;
   arr.forEach(log =>  {
+    temp += Number(invest);
     data.date.push(log[0]);
     data.price.push(log[1]);
+    data.principal.push(temp);
   })
-
+  console.log(data.principal);
   // populate the number of shares owned based off of the amounted invested
-  let temp = 0;
+  temp = 0;
   data.price.forEach(price => {
     temp += invest/price;
     data.shares.push(temp);
--- src/generateChart.js
+++ src/generateChart.js
@@ -11,16 +11,20 @@ function generateChart(obj){
           data: obj.invenstment,
           backgroundColor: 
               'rgba(255, 99, 132, 0.2)',
-          borderColor: [
+          borderColor:
               'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
-          ],
           borderWidth: 1
-      }]
+      },
+      {
+        label: 'Principal Value',
+        data: obj.principal,
+        backgroundColor: 
+            'rgba(255, 128, 142, 0.2)',
+        borderColor:
+            'rgba(255, 99, 132, 1)',
+        borderWidth: 1
+    },
+    ]
   },
   });
 }
